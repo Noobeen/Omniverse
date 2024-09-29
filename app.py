eec11679d6ba6302e2b52ba0a26fd3b84b1dd8d1
Noobeen <olinab30@gmail.com>
2024-09-29 12:14:09 -0400

    a barebone chat interface

--- app.py
+++ app.py
@@ -1,5 +1,33 @@
 import numpy as np
+import os
 import pandas as pd
+from pandasai import Agent
+from pandasai.llm.local_llm import LocalLLM
+import streamlit as st
+from key import pd_key # to get Api_key from user
 
+#Assinging API Key
+os.environ["PANDASAI_API_KEY"]=pd_key
+
+st.title("Omniverse")
+
+
+#Taking sample data and create pandas data frame
 data = pd.read_csv('txt.csv')
-print(data.head)
+
+#Creating agent
+agent = Agent(data)
+
+#Prompt
+hist=""
+prompt = st.chat_input("Say something")
+if prompt:
+    st.write(f"User has sent the following prompt: {prompt}")
+
+    a=agent.chat(prompt)
+    hist=hist+"  "+prompt
+
+    st.header(a)
+else:
+    exit
+
