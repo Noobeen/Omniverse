e2f73791c87e2c71782579e9881fdf9ba8637f4f
Noobeen <olinab30@gmail.com>
2024-10-06 13:43:08 -0400

    Added image function

--- data.py
+++ data.py
@@ -3,20 +3,22 @@ from pandasai import Agent
 from pandasai.llm.local_llm import LocalLLM
 import os
 import streamlit as st
-from langchain_ollama import ChatOllama
+#from langchain_ollama import ChatOllama
+from Locallm import llm
  
-
-llm = ChatOllama(
-    model="llama3.2:3b",
-    temperature=0
-)
-
 df=pd.read_csv('txt.csv')
 
-agent = Agent(df,config={"llm":llm})
+agent = Agent(
+    df,
+    description="You are a data analysis agent. Your main goal is to help non-technical users to analyze data",
+    config={"llm":llm}
+)
 st.title("Visualization tool")
 question = st.chat_input("Say something")
 if question:
-    st.header(agent.chat(question))
+    a=agent.chat(question)
+    if a==
+    st.write(a)
+    st.image(a)
 else:
     exit    
