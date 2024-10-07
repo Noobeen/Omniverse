6ce8398321af05fd109693525aede1a345d28084
Noobeen <olinab30@gmail.com>
2024-10-06 22:32:07 -0400

    final touches

---
+++ play_with_data.py
@@ -0,0 +1,22 @@
+import pandas as pd 
+from pandasai import Agent
+from pandasai.llm.local_llm import LocalLLM
+import os
+import streamlit as st
+from Locallm import llm
+ 
+df=pd.read_csv('txt.csv')
+agent = Agent(
+    df,
+    description="You are a data analysis agent. Your main goal is to help non-technical users to analyze data",
+config={"llm":llm},)
+st.title("Hey ask question, create graphs I have a lot data !!!")
+question = st.chat_input("Say something")
+if question:
+    a=agent.chat(question)
+    if a=="C:/Users/College_stuffs/spaceapp/Omniverse/exports/charts/temp_chart.png" :
+        st.image(a)
+    else:
+        st.write(a)
+else:
+    exit    
