bef3333954942a57aabb3328e13b33d7c6dcc94c
Noobeen <olinab30@gmail.com>
2024-10-06 22:54:58 -0400

    history of chat

---
+++ hist.py
@@ -0,0 +1,17 @@
+# this file saves chat history by creating dictionary named store 
+
+from langchain_community.chat_message_histories import ChatMessageHistory
+from langchain_community.document_loaders import CSVLoader
+from langchain_core.chat_history import BaseChatMessageHistory
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.runnables.history import RunnableWithMessageHistory
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+store={}
+
+def get_session_history(session_id: str) -> BaseChatMessageHistory:
+    if session_id not in store:
+        store[session_id] = ChatMessageHistory()
+    return store[session_id]
+
